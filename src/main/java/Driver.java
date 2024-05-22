bacac46653e7ee0cd5f966e86e6a5cff08c0f845
mzaky <mzakyf5503@gmail.com>
2024-05-22 21:20:35 +0700

    undo

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,21 +3,19 @@ import src.main.java.Counter;
 public class Driver {
 
 	public static void main(String[] args) {
-		
+
 		Counter counter = new Counter();
-		
+
 		System.out.println("Current count:" + counter.getCount());
-		
-		counter.increaseBy(5);
-		
+
+		counter.increment();
+
 		System.out.println("Current count:" + counter.getCount());
-		
-		counter.decreaseBy(2);
-		
+
+		counter.decrement();
+
 		System.out.println("Current count:" + counter.getCount());
-		
-		counter.multiplyBy(6);
-		
-		System.out.println("Current count:" + counter.getCount());	
+
 	}
+
 }
